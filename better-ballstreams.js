5ebaac80c179ac63fb90ce36368d594a771b24d1
wyaeiga <kevin2041@gmail.com>
2015-05-07 18:45:34 -0500

    adding support for live dvr

--- better-ballstreams.js
+++ better-ballstreams.js
@@ -21,10 +21,11 @@ function addGlobalStyle(css) {
 
 $(function() {
   var button_html = '<a href="#" class="button_noninput bb-toggle"></a>'
-  var iframe_padding = '<div id="iframe_padding" style="height: 50px; width: 100%; display: flex; flex: 0 1 50px;"></div>'
+  var iframe_padding_html = '<div id="iframe_padding" style="height: 50px; width: 100%; display: flex; flex: 0 1 50px;"></div>'
 
-  addGlobalStyle('.bb-player                { position: absolute !important; width: 80% !important; height: 100% !important; top: 0 !important; left: 0 !important; z-index: 99999999 !important; }')
-  addGlobalStyle('.bb-player-bg             { position: absolute !important; width: 80% !important; height: 100% !important; top: 0 !important; left: 0 !important; z-index: 99999 !important; }')
+  addGlobalStyle('.bb-player-live           { position: absolute !important; width: 80% !important; height: 100% !important; top: 0 !important; left: 0 !important; z-index: 99999999 !important; }')
+  addGlobalStyle('.bb-player-dvr            { position: absolute !important; width: 100% !important; height: 100% !important; top: 0 !important; left: 0 !important; z-index: 99999999 !important; }')
+  addGlobalStyle('.bb-player-dvr-bg         { position: absolute !important; width: 80% !important; height: 100% !important; top: 0 !important; left: 0 !important; z-index: 99999 !important; }')
   addGlobalStyle('.bb-chat                  { position: absolute !important; width: 20% !important; height: 100% !important; bottom: 0 !important; right: 0 !important; z-index: 99999998 !important; background-color: #000; padding-top: 50px !important; display: flex; flex-flow: column; }')
   addGlobalStyle('.bb-chat-iframe           { width: 100% !important; margin-left: auto; margin-right: auto; display: flex; flex: 1 1 auto; }')
   addGlobalStyle('.bb-hr                    { height: 0px !important; }')
@@ -37,18 +38,32 @@ $(function() {
   addGlobalStyle('.bb-display-none          { display: none; }')
   addGlobalStyle('.bb-on-demand-player      { width: 100% !important; height: 100% !important; position: absolute !important; top: 0 !important; left: 0 !important; z-index: 9999 !important; }')
 
-  $(".button_noninput:contains('TrueLive')").first().before(button_html)
-  $('#webchat').before(iframe_padding)
-  $('#iframe_padding').toggle()
+  var truelive_button = '.button_noninput:contains("TrueLive")'
+  var iframe_padding = '#iframe_padding'
+  var chat_frame = '#webchat'
+  var bb_toggle = '.bb-toggle'
+  var live_player = '#mediaplayer_wrapper'
+  var dvr_bg = '#main'
+  var dvr_player = 'object[name="player"]'
+  var chat_div = '#playerBottom'
+  var chat_div_hr = chat_div+'>hr'
+  
+  $(truelive_button).first().before(button_html)
+  $(chat_frame).before(iframe_padding_html)
+  $(iframe_padding).toggle()
+  
 
-  $('.bb-toggle').click(function(){
-    $('#mediaplayer_wrapper').toggleClass('bb-player')
-    $('#main').toggleClass('bb-player-bg')
+  $(bb_toggle).click(function(){
+    $(live_player).toggleClass('bb-player-live')
+    $(dvr_bg).toggleClass('bb-player-dvr-bg')
+    $(dvr_player).toggleClass('bb-player-dvr')
+    
     $('body').toggleClass('hide-scrollbar')
-    $('.bb-toggle').toggleClass('bb-toggle-close')
-    $('#playerBottom').toggleClass('bb-chat')
-    $('#playerBottom>hr').toggleClass('bb-hr')
-    $('#webchat').toggleClass('bb-chat-iframe')
-    $('#iframe_padding').toggle()
+    $(bb_toggle).toggleClass('bb-toggle-close')
+    
+    $(chat_div).toggleClass('bb-chat')
+    $(chat_div_hr).toggleClass('bb-hr')
+    $(chat_frame).toggleClass('bb-chat-iframe')
+    $(iframe_padding).toggle()
   });
 });

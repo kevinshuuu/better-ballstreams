098c927b63f94616f5b233024c5abc7062270809
wyaeiga <kevin2041@gmail.com>
2015-05-07 18:46:07 -0500

    Update better-ballstreams.js

--- better-ballstreams.js
+++ better-ballstreams.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better Ballstreams
 // @namespace    https://github.com/wyaeiga/better-ballstreams
-// @version      0.3
+// @version      0.4
 // @description  Transform the default Ballstreams view into a theater mode with the video blown up to the left and the chat full height on the right.
 // @author       Kevin Shu
 // @match        http://*.ballstreams.com/*
